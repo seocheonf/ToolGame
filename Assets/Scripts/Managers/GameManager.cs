d638149d0be7b3ba36b2c54b99a440bfc5c769d9
Hngmo.Kim <seocheonf@gmail.com>
2024-09-24 17:44:10 +0900

    UIManager - UI System 3차 진행본 (델리게이트 실행 도중 델리게이트가 추가되어 null로 값을 넣어줄 시 신규 델리게이트 함수를 무시하는 현상 수정중)

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -40,8 +40,58 @@ public class GameManager : MonoBehaviour
     //실행의 순서를 지켜야 하는 묶음단위가 있으므로 각각의 델리게이트로 분리하여 관리한다.
 
     //처음 나타날 때 한번 해야할 일들의 묶음
-    public static StartFunction ManagersStart;
-    public static StartFunction ObjectsStart;
+    public static StartFunction ManagersStart
+    {
+        get
+        {
+
+            while (ManagersStart_A != null || ManagersStart_B != null)
+            {
+                //양쪽 중 하나라도 비어있지 않다면
+
+                if (managersStart_Running)
+                {
+                    ManagersStart_A?.Invoke();
+                    ManagersStart_A = null;
+                }
+                else
+                {
+                    ManagersStart_B?.Invoke();
+                    ManagersStart_B = null;
+                }
+
+                managersStart_Running = !managersStart_Running;
+
+            }
+            //만약 양쪽 다 비었다면 그만 실행.
+            return null;
+
+        }
+        set
+        {
+            if(value == null)
+            {
+                ManagersStart_A = null;
+                ManagersStart_B = null;
+            }
+
+            if (!managersStart_Running)
+                ManagersStart_A = value;
+            else
+                ManagersStart_B = value;
+        }
+    }
+    private static bool managersStart_Running = true; // get 했을 시 현재 실행해야 할 트리거 [true : A, false : B]. set은 그 반대
+    private static StartFunction ManagersStart_A;
+    private static StartFunction ManagersStart_B;
+
+    public static StartFunction ObjectsStart
+    {
+        get;
+        set;
+    }
+    private static StartFunction ObjectsStart_A;
+    private static StartFunction ObjectsStart_B;
 
     //매 프레임마다 지속적으로 해야할 일들의 묶음
     public static UpdateFunction ManagersUpdate;
